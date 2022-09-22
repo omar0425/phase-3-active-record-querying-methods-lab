375ba6aeb398eb79853c930ff506f2042238cd54
omar0425 <startfresh87@gmail.com>
2022-09-22 00:17:26 -0400

    commit tree bc71fcd38556b0c2fc8561f78405afc3723d080d

---
+++ app/models/show.rb
@@ -0,0 +1,11 @@
+class Show < ActiveRecord::Base
+
+  def self.highest_rating
+    Show.maximum(:rating)
+  end
+
+
+  def self.most_popular_show
+    self.where("rating = ?", self.highest_rating).first
+  end
+end
